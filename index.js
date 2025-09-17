8850a10883e547e77b724e01891a0eb3e417a9a8
khan$ <jabborovhasan853@gmail.com>
2025-09-17 23:40:34 +0900

    EJS farmework orqali ananaviy frontend quramiz

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background:red" >Hello World  by Bekhzodbek</h1>`);
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1 style="background:red" >Hello World  by Bekhzodbek</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1>You are page of Gift</h1>`);
+// });
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end(`<h1>You are page of Gift</h1>`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
