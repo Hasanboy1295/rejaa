109946fb11dd3941879f278736dfb10d15f541e4
khan$ <jabborovhasan853@gmail.com>
2025-09-27 11:00:08 +0900

    feet:  tasks

--- train.js
+++ train.js
@@ -1,13 +1,39 @@
-console.log("train task ishga tushdi");
-console.log("Jack ma maslaahatlari")
-const list = [
-    "yahwi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq xato qling", // 20-30
-    "uzingizga ishlashni boshlang",//30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga investitiya qiling", //50-60
-    "endi dam oling, fooydasi yoq endu", //60
-]
+// TASK B
+
+//const { text } = require("express");
+
+// Shunday, function tuzingki, bu function yagona parametrga ega bo'lib
+// string tarkibidagi sonlar miqdorini qaytarsin
+
+
+
+  function countDigits(dev) {
+    let div = dev.split("");
+    let digits = dev.filter(function(ele) {
+        return  ele >= "0" && ele <= "9";
+    });
+    return digits(countDigits("ad2a54y79wet0sfgb9"));
+  }
+  console.log
+  // 4. Funksiyan
+// Masalan: countDigits("ad2a54y79wet0sfgb9")
+// Yuqoridagi string tarkibida 7 dona raqam qatnashganligi uchun, natija 7 qaytadi
+
+//let str = "ad2a54y79wet0sfgb9";
+//let number = str.split('').filter(char => !isNaN(char) && char !== ' ').join('');
+//console.log(number);
+
+
+
+// console.log("train task ishga tushdi");
+// console.log("Jack ma maslaahatlari")
+// const list = [
+//     "yahwi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq xato qling", // 20-30
+//     "uzingizga ishlashni boshlang",//30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga investitiya qiling", //50-60
+//     "endi dam oling, fooydasi yoq endu", //60]
 // function maslahatBering(a, callback) {
 //     if(typeof a !== "number") callback("insert a number", null);
 //     else if(a <= 20) callback( null, list[0]);
@@ -22,16 +48,16 @@ const list = [
 //    }
 // }
 //CALLBACK function 
- async function maslahatBering(a, callback)  { 
-    if (typeof a !== "number") throw new Error ("Insert a number" );
-    else if(a <= 20) return list [0];
-    else if (a > 20 && a <= 30)  return list [1];
-    else if (a > 30 && a <= 40)  return list [2];
-    else if (a > 40 && a <= 50)  return list [3];
-    else if (a > 50 && a <= 60)  return list [4];
-else { setInterval(function () {
-            callback(null, list[5]);
-      }, 5000);
+//  async function maslahatBering(a, callback)  { 
+//     if (typeof a !== "number") throw new Error ("Insert a number" );
+//     else if(a <= 20) return list [0];
+//     else if (a > 20 && a <= 30)  return list [1];
+//     else if (a > 30 && a <= 40)  return list [2];
+//     else if (a > 40 && a <= 50)  return list [3];
+//     else if (a > 50 && a <= 60)  return list [4];
+// else { setInterval(function () {
+//             callback(null, list[5]);
+//       }, 5000);
          //return new Promise ((resolve, reject) => {
         // setInterval(() =>{ //Timeout va Interval
         //     resolve(list[1]);
@@ -40,8 +66,8 @@ else { setInterval(function () {
     // setTimeout(function() {
     //     return list [5];
     // }, 1000);
-}
-}
+// }
+// }
  // ASYNC function
 // console.log("passed here 0");     THEN AND CATCH
 //.          call via THEN AND CATCH
@@ -69,29 +95,30 @@ else { setInterval(function () {
     
 // }
 // run();
-     console.log("passed here 0");
-     maslahatBering(70, (err, data) => {
-        if (err) console.log("ERROR:", err);
-        else {
-            console.log(data);
-        }
-     });
-     console.log("passed here 1")
-     maslahatBering(40, (err, data) => {
-        if (err) console.log("ERROR:", err);
-        else {
-            console.log(data);
-        }
-     });
-     console.log("passed here 1")
+    //  console.log("passed here 0");
+    //  maslahatBering(70, (err, data) => {
+    //     if (err) console.log("ERROR:", err);
+    //     else {
+    //         console.log(data);
+    //     }
+    //  });
+    //  console.log("passed here 1")
+    //  maslahatBering(40, (err, data) => {
+    //     if (err) console.log("ERROR:", err);
+    //     else {
+    //         console.log(data);
+    //     }
+    //  });
+    //  console.log("passed here 1")
 
 
      //TASK
 
       
-    function countLetter(word, letter) {
-        return word.split("").filter(s  =>  s === letter).length;
-      }
+    // function countLetter(word, letter) {
+    //     return word.split("").filter(s  =>  s === letter).length;
+    //   }
       
-      console.log(countLetter("scissor", "s")); 
+    //   console.log(countLetter("scissor", "s")); 
       
+
