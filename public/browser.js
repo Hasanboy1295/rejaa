f76af626c9ef63336daa8b93858db8b67f001c8c
Hasanboy1295 <jabborovhasan853@gmail.com>
2025-10-02 18:41:18 +0900

    feat:helllo

--- public/browser.js
+++ public/browser.js
@@ -1,84 +1,86 @@
-console.log("FrontEnd Js Ishga Tushdi")
+console.log("FrontEnd JS ishga tushdi");
 
 function itemTemplate(item) {
-    return ` <li 
-    class="list-group-item    list-group-item-info d-flex align-items-center justify-content-between">
-  <span class="item-text">${item.reja}</span>
-<div>
- 
-  <button   data-id="${item.id}"  class="edit-me btn btn-secondary btn-sm mr-1"> Ozgartrish </button>
- <button 
-   data-id="${item.id}" 
-   class="delete-me btn btn-danger btn-sm" >
-  Ochrish
-  </button>
-</div>
-</li>`;
-}
+    return `<li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
+                    <span class="item-text">${item.reja}</span>
+                    <div>
+                        <button 
+                            data-id="${id=item._id}"
+                            class="edit-me btn btn-secondary btn-sm mr-1">
+                                 Ozgartirish
+                        </button>
+                        <button 
+                            data-id="${item._id}" 
+                            class="delete-me btn btn-danger btn-sm">
+                                Ochirish
+                        </button>
+                    </div>
+                    </li>`; 
+};
 
-let createField = document.getElementById("create-field");
-document.getElementById("create-form") 
-.addEventListener("submit", function (e) { 
-    e.preventDefault();
-    axios
-    .post("/create-item", { reja: createField.value })
-    .then((response) => {
-        document
-        .getElementById("item-list")
-        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-    createField.value = "";
-    createField.focus(); 
- })
-    .catch((err) => { 
-      console.log("Iltmos qaytadan h-t qilib koring");
+let createField = document.getElementById("create-field"); 
+
+document
+    .getElementById("create-form")
+    .addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        axios.post("/create-item", {reja: createField.value})
+        .then((response) => {
+            document
+                .getElementById("item-list")
+                .insertAdjacentHTML("beforeend", itemTemplate(response.data))
+            createField.value = "";
+            createField.focus();
+        })
+        .catch((err) => {
+            console.log("Iltimos qaytadan harakat qiling!"); 
+        });
 });
-});
-document.addEventListener("click", function(e) {
-  //delete oper
-console.log(e.target)
-  if(e.target.classList.contains("delete-me")) {
-   
-    if(confirm("Aniq ochirmoqchimisiz?")) {
-      axios
-      .post("/delete-item", { id: e.target.getAttribute("data-id") })
-      .then((responce) => {
-        console.log(responce.data );
-        e.target.parentElement.parentElement.remove();
-      })
-      .catch((err) =>{
-        console.log("Iltmos qaytadan h-t qilib koring");
-      });
+
+
+document.addEventListener("click", function(e)   {
+    console.log(e.target)
+    if(e.target.classList.contains("delete-me")) {
+       if(confirm("Aniq o'chirmoqchismiz?")) {
+        axios
+        .post("/delete-item", {id: e.target.getAttribute("data-id")})
+        .then((response) => {
+            console.log(response.data);
+            e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+           console.log("Iltimos qaytadan harakat qiling!"); 
+
+        })
+        
+       } 
     }
-  }
 
-  //edit oper
-  if(e.target.classList.contains("edit-me")) {
-   let userInput = prompt(
-    "Yangi O'zgartrish kriying", 
-    e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
-    if(userInput) {
-      axios.post("/edit-item", {
-        id: e.target.getAttribute("data-id"),
-         new_input: userInput,
-    })
-    .then(response => {
-console.log(response.data);
- e.target.parentElement.parentElement.querySelector(
- ".item-text"
- ).innerHTML = userInput
-    })
-    .catch(err => { 
-console.log("Iltmos Qaytadan h-t qiling!")
-    });
-     
+
+    if(e.target.classList.contains("edit-me")) {
+        let userInput = prompt("O'zgartirish kitiring", 
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+       if(userInput){
+            axios.post("/edit-item", 
+            {id: e.target.getAttribute("data-id"), 
+            new_input: userInput})
+            .then((response) => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.querySelector(".item-text")
+                .innerHTML = userInput;
+            })   
+            .catch((err) => {
+                console.log("Iltimos qaytadan harakat qiling!"); 
+            })
+        }
     }
-  }
 });
 
 
-document.getElementById("clean-all").addEventListener("click", function () {
-  axios.post("/delete-all", { delete_all: true }).then((response) => {
-    alert(response.data.state);
-    document.location.reload();
-  });
-});
+document.getElementById("clean-all").addEventListener("click", function(){
+    axios.post("/delete-all", {delete_all: true}).then(response => {
+        alert(response.data.state);
+        document.location.reload();
+    })
+})
