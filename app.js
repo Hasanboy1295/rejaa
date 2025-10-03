8f3b27ad566e494e72d9038161b3f0b4620b956d
Hasanboy1295 <jabborovhasan853@gmail.com>
2025-10-03 11:20:49 +0900

    feet: edit

--- app.js
+++ app.js
@@ -1,96 +1,91 @@
-//console.log("Web serverni boshlash");
+console.log("Web Serverlarni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
 
-const express = require('express');
-const res = require('express/lib/response');
-const app = express();  // app --> Object
-const fs = require('fs');
-const { console } = require('inspector');
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
-
-// MongoDB Connection 
+//MongoDb
 const db = require("./server").db();
 const mongodb = require("mongodb");
 
-
-let user;   
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
-    }
-})
-
-
-// Section - 1 (Enterence)
-
+//1 Kirish kodlari
 app.use(express.static("public"));
-app.use(express.json());  // json format --> Object
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Section - 2 (Session)
-
-
-
-// Section - 3 (Views - BSSR)
-
+// 2 Session code
+//3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// Section - 4 (Routing)
-
+// 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log("User entered /create-item");
-    const new_reja = req.body.reja; 
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        console.log(data.ops);
-        res.json(data.ops[0]);
-    });
+  console.log("user entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    res.json({ _id: data.insertedId,  reja: new_reja});
+  });
+  //   console.log(req); //body
+  // res.json({ test: "success" });
 });
-
 app.post("/delete-item", (req, res) => {
-    const id = req.body.id;
-    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, 
-    function(err, data) {
-        res.json({state: "success"});
+  console.log("req.body:", req.body.id);
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
     }
-    );
+  );
 });
 
 app.post("/edit-item", (req, res) => {
-    const data = req.body;
-    console.log(data);
-    db.collection("plans").findOneAndUpdate(
-        {_id: new mongodb.ObjectId(data.id)}, 
-        {$set: {reja: data.new_input}}, 
-        function(err, data){
-            res.json({state: "success"});
-        })
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
 });
 
 app.post("/delete-all", (req, res) => {
-    if(req.body.delete_all) {
-        db.collection("plans").deleteMany(function() {
-            res.json({state: "Hamma rejalar o'chirildi"});
-        });
-    }
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "hamma rejalar o'chirildi" });
+    });
+  }
 });
 
-app.get('/author', (req, res) => {
-    res.render('author', { user: user });
-})
-
-app.get("/", function(req, res) {
-    console.log("User entered /")
-    db.collection("plans").find().toArray( (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("Somthing went wrong")
-        } else {    
-            res.render('reja', { items: data  });
-        }
+app.get("/", function (req, res) {
+  // { user: user })
+  console.log("user entered / ");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        console.log(data);
+        res.render("reja", { items: data });
+      }
     });
 });
 
+app.get("/author", (req, res) => {
+  const user = { name: "Albert", age: 24 };
+  res.render("author", { user });
+});
 
 module.exports = app;
--- public/browser.js
+++ public/browser.js
@@ -1,86 +1,84 @@
-console.log("FrontEnd JS ishga tushdi");
+console.log("FrontEnd Js Ishga Tushdi");
+
+const createField = document.getElementById("create-field");
+const deleteAll = document.getElementById("clean-all");
 
 function itemTemplate(item) {
-    return `<li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-                    <span class="item-text">${item.reja}</span>
-                    <div>
-                        <button 
-                            data-id="${id=item._id}"
-                            class="edit-me btn btn-secondary btn-sm mr-1">
-                                 Ozgartirish
-                        </button>
-                        <button 
-                            data-id="${item._id}" 
-                            class="delete-me btn btn-danger btn-sm">
-                                Ochirish
-                        </button>
-                    </div>
-                    </li>`; 
-};
+  return ` <li 
+    class="list-group-item    list-group-item-info d-flex align-items-center justify-content-between">
+  <span class="item-text">${item.reja}</span>
+<div>
+ 
+  <button   data-id="${item._id}"  class="edit-me btn btn-secondary btn-sm mr-1"> Ozgartrish </button>
+ <button 
+   data-id="${item._id}" 
+   class="delete-me btn btn-danger btn-sm" >
+  Ochrish
+  </button>
+</div>
+</li>`;
+}
 
-let createField = document.getElementById("create-field"); 
-
-document
-    .getElementById("create-form")
-    .addEventListener("submit", function(e) {
-        e.preventDefault();
-
-        axios.post("/create-item", {reja: createField.value})
-        .then((response) => {
-            document
-                .getElementById("item-list")
-                .insertAdjacentHTML("beforeend", itemTemplate(response.data))
-            createField.value = "";
-            createField.focus();
-        })
-        .catch((err) => {
-            console.log("Iltimos qaytadan harakat qiling!"); 
-        });
-});
-
-
-document.addEventListener("click", function(e)   {
-    console.log(e.target)
-    if(e.target.classList.contains("delete-me")) {
-       if(confirm("Aniq o'chirmoqchismiz?")) {
-        axios
-        .post("/delete-item", {id: e.target.getAttribute("data-id")})
-        .then((response) => {
-            console.log(response.data);
-            e.target.parentElement.parentElement.remove();
-        })
-        .catch((err) => {
-           console.log("Iltimos qaytadan harakat qiling!"); 
-
-        })
-        
-       } 
-    }
-
-
-    if(e.target.classList.contains("edit-me")) {
-        let userInput = prompt("O'zgartirish kitiring", 
-        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
-       if(userInput){
-            axios.post("/edit-item", 
-            {id: e.target.getAttribute("data-id"), 
-            new_input: userInput})
-            .then((response) => {
-                console.log(response.data);
-                e.target.parentElement.parentElement.querySelector(".item-text")
-                .innerHTML = userInput;
-            })   
-            .catch((err) => {
-                console.log("Iltimos qaytadan harakat qiling!"); 
-            })
-        }
-    }
-});
-
-
-document.getElementById("clean-all").addEventListener("click", function(){
-    axios.post("/delete-all", {delete_all: true}).then(response => {
-        alert(response.data.state);
-        document.location.reload();
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  axios
+    .post("/create-item", { reja: createField.value })
+    .then((response) => {
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+      createField.value = "";
+      createField.focus();
     })
-})
+    .catch((err) => {
+      console.log("Iltimos qaytadan urinib koring");
+    });
+});
+
+document.addEventListener("click", function (e) {
+  //delete oper
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisiz?")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }
+
+  //  edit oper
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt(
+      "Yangi O'zgartrish kriying",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltmos Qaytadan h-t qiling!");
+        });
+    }
+  }
+});
+deleteAll.addEventListener("click", () => {
+    axios.post("/delete-all", { delete_all: true }).then((response) => {
+      alert(response.data.state);
+      document.location.reload();
+    });
+  });
